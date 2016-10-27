f5cd4bb22b82157b4f011154a9ddae00fe11059d
JimeCB <jime_castaneda@hotmail.com>
2016-10-27 18:56:18 -0500

    update

--- js/function.js
+++ js/function.js
@@ -1,112 +1,25 @@
 
-function change_text1 (){
-	document.getElementById("about-title").innerHTML = "OUR PURPOSE";
-	var x = document.getElementById ("about1").innerHTML;
-	document.getElementById("about").innerHTML = x;
-	
-	document.getElementById('icon-h1').style.color='#ff80b2';
-	document.getElementById('icon-h2').style.color='black';
-	document.getElementById('icon-h3').style.color='black';
-	document.getElementById('icon-h4').style.color='black';
+function change_text (index){
+	document.getElementById("about-title").innerHTML = document.getElementById("about-title" + index).innerHTML;
+	document.getElementById("about").innerHTML = document.getElementById("about" + index).innerHTML;
+
+	for(var i = 1; i < 5; i++)
+		document.getElementById('icon-h' + i).style.color = (i == index) ? '#ff80b2' : "black";
 }
 
-function change_text2 (){
-	document.getElementById("about-title").innerHTML = "OUR MISION";
-	var x = document.getElementById ("about2").innerHTML;
-	document.getElementById("about").innerHTML = x;
-	
-	document.getElementById('icon-h1').style.color='black';
-	document.getElementById('icon-h2').style.color='#ff80b2';
-	document.getElementById('icon-h3').style.color='black';
-	document.getElementById('icon-h4').style.color='black';
-}
+function change_img(index) {
+	if (index < 5) {
+		document.getElementById('img_model1').src = 'images/model'+index+'.png';
+		for(var i = 1; i < 5; i++) {
+			document.getElementById('icon-v' + i).style.color = (i == index) ? '#ff80b2' : "black";}
+	}else{
+		document.getElementById('img_model2').src = 'images/model'+index+'.png';
+		for(var i = 5; i < 9; i++) {
+			document.getElementById('icon-v' + i).style.color = (i == index) ? '#ff80b2' : "black";}
+	}
 
-function change_text3 (){
-	document.getElementById("about-title").innerHTML = "OUR VISION";
-	var x = document.getElementById ("about3").innerHTML;
-	document.getElementById("about").innerHTML = x;
-	
-	document.getElementById('icon-h1').style.color='black';
-	document.getElementById('icon-h2').style.color='black';
-	document.getElementById('icon-h3').style.color='#ff80b2';
-	document.getElementById('icon-h4').style.color='black';
-}
 
-function change_text4 (){
-	document.getElementById("about-title").innerHTML = "ABOUT US";
-	var x = document.getElementById ("about4").innerHTML;
-	document.getElementById("about").innerHTML = x;
-	
-	document.getElementById('icon-h1').style.color='black';
-	document.getElementById('icon-h2').style.color='black';
-	document.getElementById('icon-h3').style.color='black';
-	document.getElementById('icon-h4').style.color='#ff80b2';
 }
 
 
-function change_img1() {
-    document.getElementById('img_model1').src='images/model1.png';
-	document.getElementById('icon-v1').style.color='#ff80b2';
-	document.getElementById('icon-v2').style.color='black';
-	document.getElementById('icon-v3').style.color='black';
-	document.getElementById('icon-v4').style.color='black';
-}
-
-function change_img2() {
-    document.getElementById('img_model1').src='images/model2.png';
-	document.getElementById('icon-v1').style.color='black';
-	document.getElementById('icon-v2').style.color='#ff80b2';
-	document.getElementById('icon-v3').style.color='black';
-	document.getElementById('icon-v4').style.color='black';
-}
-
-function change_img3() {
-    document.getElementById('img_model1').src='images/model3.png';
-	document.getElementById('icon-v1').style.color='black';
-	document.getElementById('icon-v2').style.color='black';
-	document.getElementById('icon-v3').style.color='#ff80b2';
-	document.getElementById('icon-v4').style.color='black';
-}
-
-function change_img4() {
-    document.getElementById('img_model1').src='images/model4.png';
-	document.getElementById('icon-v1').style.color='black';
-	document.getElementById('icon-v2').style.color='black';
-	document.getElementById('icon-v3').style.color='black';
-	document.getElementById('icon-v4').style.color='#ff80b2';
-}
-
-
-
-function change_img5() {
-    document.getElementById('img_model2').src='images/model5.png';
-	document.getElementById('icon-v5').style.color='#ff80b2';
-	document.getElementById('icon-v6').style.color='black';
-	document.getElementById('icon-v7').style.color='black';
-	document.getElementById('icon-v8').style.color='black';
-}
-
-function change_img6() {
-    document.getElementById('img_model2').src='images/model6.png';
-    document.getElementById('icon-v5').style.color='black';
-	document.getElementById('icon-v6').style.color='#ff80b2';
-	document.getElementById('icon-v7').style.color='black';
-	document.getElementById('icon-v8').style.color='black';
-}
-
-function change_img7() {
-    document.getElementById('img_model2').src='images/model7.png';
-	document.getElementById('icon-v5').style.color='black';
-	document.getElementById('icon-v6').style.color='black';
-	document.getElementById('icon-v7').style.color='#ff80b2';
-	document.getElementById('icon-v8').style.color='black';
-}
-
-function change_img8() {
-    document.getElementById('img_model2').src='images/model8.png';
-	document.getElementById('icon-v5').style.color='black';
-	document.getElementById('icon-v6').style.color='black';
-	document.getElementById('icon-v7').style.color='black';
-	document.getElementById('icon-v8').style.color='#ff80b2';
-}
 
