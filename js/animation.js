e19fb1eebf3cd08dd9979be25208ee7a6dd3c187
JimeCB <jime_castaneda@hotmail.com>
2016-11-02 12:02:22 -0600

    shadow

--- js/animation.js
+++ js/animation.js
@@ -2,35 +2,54 @@
  * Created by Jimena on 31/10/2016.
  */
 
+
+var perspectiveWrapper = document.getElementById( 'perspective' ),
+    container = perspectiveWrapper.querySelector( '.container' ),
+    contentWrapper = container.querySelector( '.wrapper' );
+
+
+
+function scrollY() {
+    return window.pageYOffset || window.document.documentElement.scrollTop;
+}
+
+
+
+
 $(document).ready(function(){
 
-    $('#icon-menu').click(function () {
+    $('#icon-menu, #home-mobile').click(function () {
+
+        docScroll = scrollY();
+
         $('.container').toggleClass('animate');
+
+        $('.shadow').toggleClass('animate-shadow');
+
         $('.perspective').toggleClass('modalview');
+        $('.wrapper').toggleClass('wrapper-animate');
         $('.nav').toggleClass('nav-animate');
+
+        $('.container').scrollTop(docScroll);
     });
 
-    $( '.container, #home-link' ).click(function() {
-        if ($('.container').hasClass('animate')){
-            $('.container').removeClass('animate');
-            $('.perspective').removeClass('modalview');
-            $('.nav').removeClass('nav-animate');
-        }
-    });
-
-    $( '#about-link' ).click(function() {
+    $( '#about-mobile' ).click(function() {
         $('.container').removeClass('animate');
         $('.perspective').removeClass('modalview');
+        $('.wrapper').removeClass('wrapper-animate');
         $('.nav').removeClass('nav-animate');
+        $('.shadow').removeClass('animate-shadow');
         setTimeout(function(){
             $('html,body').scrollTop($('#bm-about').offset().top -25);
         }, 500);
     });
 
-    $( '#models-link' ).click(function() {
+    $( '#models-mobile' ).click(function() {
         $('.container').removeClass('animate');
         $('.perspective').removeClass('modalview');
+        $('.wrapper').removeClass('wrapper-animate');
         $('.nav').removeClass('nav-animate');
+        $('.shadow').removeClass('animate-shadow');
         setTimeout(function(){
             $('html,body').scrollTop($('#bm-models').offset().top -25);
         }, 500);
