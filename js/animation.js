867d6986c18d520af569875de409a8036f2b50ce
JimeCB <jime_castaneda@hotmail.com>
2016-11-02 17:57:57 -0600

    superior bar included in animation

--- js/animation.js
+++ js/animation.js
@@ -1,31 +1,15 @@
-/**
- * Created by Jimena on 31/10/2016.
- */
-
-
-var perspectiveWrapper = document.getElementById( 'perspective' ),
-    container = perspectiveWrapper.querySelector( '.container' ),
-    contentWrapper = container.querySelector( '.wrapper' );
-
-
 
 function scrollY() {
     return window.pageYOffset || window.document.documentElement.scrollTop;
 }
 
-
-
-
 $(document).ready(function(){
 
     $('#icon-menu, #home-mobile').click(function () {
-
         docScroll = scrollY();
 
         $('.container').toggleClass('animate');
-
         $('.shadow').toggleClass('animate-shadow');
-
         $('.perspective').toggleClass('modalview');
         $('.wrapper').toggleClass('wrapper-animate');
         $('.nav').toggleClass('nav-animate');
@@ -33,26 +17,17 @@ $(document).ready(function(){
         $('.container').scrollTop(docScroll);
     });
 
-    $( '#about-mobile' ).click(function() {
-        $('.container').removeClass('animate');
-        $('.perspective').removeClass('modalview');
-        $('.wrapper').removeClass('wrapper-animate');
-        $('.nav').removeClass('nav-animate');
-        $('.shadow').removeClass('animate-shadow');
-        setTimeout(function(){
-            $('html,body').scrollTop($('#bm-about').offset().top -25);
-        }, 500);
-    });
+    $( '#about-mobile, #models-mobile, .wrapper' ).click(function(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
 
-    $( '#models-mobile' ).click(function() {
-        $('.container').removeClass('animate');
-        $('.perspective').removeClass('modalview');
-        $('.wrapper').removeClass('wrapper-animate');
-        $('.nav').removeClass('nav-animate');
-        $('.shadow').removeClass('animate-shadow');
-        setTimeout(function(){
-            $('html,body').scrollTop($('#bm-models').offset().top -25);
-        }, 500);
+        if($('.container').hasClass('animate')){
+            $('.container').removeClass('animate');
+            $('.perspective').removeClass('modalview');
+            $('.wrapper').removeClass('wrapper-animate');
+            $('.nav').removeClass('nav-animate');
+            $('.shadow').removeClass('animate-shadow');
+        }
     });
 
 });
