141e0385bddbb6b25f18dbbf7cc411d8f405c590
JimeCB <jime_castaneda@hotmail.com>
2016-11-05 14:01:58 -0600

    full bleed model section

--- js/function.js
+++ js/function.js
@@ -10,13 +10,15 @@ function change_text (index){
 
 function change_img(index) {
 	if (index < 5) {
-		document.getElementById('img_model1').src = 'images/model' + index + '.png';
+		$('#img_model1').css('background','url(images/model' + index + '.png)');
 		for(var i = 1; i < 5; i++) {
 			document.getElementById('icon-v' + i).style.background = (i == index) ? '#ff80b2' : "black";}
-	}else {
-		document.getElementById('img_model2').src = 'images/model' + index + '.png';
+	}else if (4 < index < 9){
+		$('#model-mobile').css('background','url(images/model' + index + '.png) no-repeat').css('background-size','cover');
 		for (var i = 5; i < 9; i++) {
 			document.getElementById('icon-v' + i).style.background = (i == index) ? '#ff80b2' : "black";}
+	}else if( 8 < index < 13 ){
+
 	}
 }
 
