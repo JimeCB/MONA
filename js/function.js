ccdd2ece410cf7b1c6e53b3188f64f7aa1d2c5ee
JimeCB <jime_castaneda@hotmail.com>
2016-11-07 00:47:50 -0600

    full bleed images

--- js/function.js
+++ js/function.js
@@ -13,15 +13,19 @@ function change_img(index) {
 		$('#img_model1').css('background','url(images/model' + index + '.png)');
 		for(var i = 1; i < 5; i++) {
 			document.getElementById('icon-v' + i).style.background = (i == index) ? '#ff80b2' : "black";}
-	}else if (4 < index < 9){
-		$('#model-mobile').css('background','url(images/model' + index + '.png) no-repeat').css('background-size','cover');
+	}else if (4 < index){
+		$('#img_model2').css('background','url(images/model' + index + '.png) no-repeat').css('background-size','cover');
 		for (var i = 5; i < 9; i++) {
 			document.getElementById('icon-v' + i).style.background = (i == index) ? '#ff80b2' : "black";}
-	}else if( 8 < index < 13 ){
-
 	}
 }
 
+function change_img2(index) {
+	$('#model-mobile').css('background','url(images/model' + index + '.png) no-repeat').css('background-size','cover');
+	for(var i = 9; i < 13; i++) {
+		document.getElementById('icon-v' + i).style.background = (i == index) ? '#ff80b2' : "black";}
+
+}
 
 
 
